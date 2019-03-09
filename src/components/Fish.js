da49ef102bfe5403bab1d251b1ced3e9a655471f
M4T <mateo.lesiuk@gmail.com>
2019-03-09 12:30:04 +0100

    Add method to addtoorder in app and add onclick in fish to trigger this method

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -5,6 +5,7 @@ class Fish extends React.Component {
   render() {
     const { image, name, desc, price, status } = this.props.details;
 
+    const isAvailable = status === 'available';
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -13,7 +14,15 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Order</button>
+        <button
+          type="button"
+          disabled={!isAvailable}
+          onClick={() => {
+            this.props.addToOrder(this.props.index);
+          }}
+        >
+          {isAvailable ? 'Add to Order' : 'Sold Out!'}
+        </button>
       </li>
     );
   }
